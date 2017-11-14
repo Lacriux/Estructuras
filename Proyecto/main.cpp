9fc861e3cb4c2332301823ca3ee7b364ae5c8fab
Lacriux <Lacriux@githob.com>
2017-11-13 23:36:48 -0600

    Proyecto terminado y comentado

--- Proyecto/main.cpp
+++ Proyecto/main.cpp
@@ -1,6 +1,18 @@
+/*Proyecto de Estructuras de datos
+    Juan Pablo Andrade Gonzalez - B40375
+    Prof. Roberto Rodriguez
+  Descripcion Breve: Este proyecto consiste en la implementacion de un juego simple de "Snake"
+
+  Se puede encontrar una descripcion mas detallada de algunas funciones en el archivo solid.h
+
+  Este archivo de encarga de crear el juego yb de llamar a las distintas funciones necesarias
+  para el funcionamiento de este, asi como crear el "loop" que permite llevar a cabo los movimientos
+  del juego.
+*/
+
 #include<iostream>
-#include<opencv2/opencv.hpp>
-#include<unistd.h>
+#include<opencv2/opencv.hpp>//Biblioteca para el uso de interfaz gráfica
+#include<unistd.h>//Biblioteca para el uso de la funcion usleep, qie se encarga de detener momentaneamente el tiempo de ejecucion
 #include"solid.h"
 
 using namespace std;
@@ -13,27 +25,27 @@ int main(){
   snake.gridToImage();
   snake.printImage();
   bool flag = true;
+  /*El siguiente loop se encarga de que el juego continue mientras no se haya
+  perdido, de lo contrario solo se mostraria una imagen y no se podria jugar*/
   while(flag){
-    dir = waitKey(10);
+    dir = waitKey(10); //Se busca si se ha presionado alguna tecla, leida en codigo ASCII
 
-    snake.changeDirection(dir);
+    snake.changeDirection(dir); //Si se ha presionado alguna tecla se envia para evaluar si esta supone un cambio de direccion
 
-    snake.move();
-    snake.gridToImage();
-    snake.printImage();
+    snake.move();//Se mueve la serpiente una casilla en la direccion actual
+    snake.gridToImage();//Se crea la imagen corrspondiente a este movimiemto anterior
+    snake.printImage();//Se imprime la imagen creada
 
+    //Si la serpiente ya no sigue viva(el jugador perdio), se rompe el ciclo del while
     if(!snake.isAlive()){
       flag = false;
     }
-
+    /*Se espera el tiempo indicado para dar al jugador oportunidad de reaccionar
+      este tiempo es medido en microsegundos, el tiempo de espera son 0,3s
+    */
     usleep(300000);
   }
 
-  snake.printScore();
-  snake.destroyImage();
-
-  //Convertir a imagen
-  //imprimir imagen
-  //preguntar por cambio de direccion
-  //esperar medio segundo
+  snake.printScore();//Una vez se termina el ciclo, se imprime la puntuacion total de la ronda
+  snake.destroyImage();//Se llama al destructor de la imagen
 }
--- Proyecto/solid.h
+++ Proyecto/solid.h
@@ -1,26 +1,50 @@
-/*En la matriz grid:
-    Si hay un 0 es una posicion vacía
-    Si hay un 1 es parte del cuerpo del Snake
-    Si hay un 2 es el punto que hay que recoger
-    Si hay un 3 es una pared
+/*Proyecto de Estructuras de datos
+    Juan Pablo Andrade Gonzalez - B40375
+    Prof. Roberto Rodriguez
+  Descripcion Breve: Este proyecto consiste en la implementacion de un juego simple de "Snake"
 
-  Para controlar la direccion se usa un pair en el que las posiciones son el incremento en la direccion Y,x.
-  Estos pueden tomar 3 valores:
-    Si el valor es -1 se mueve para arriba o para la izquierda
-    Si el valor es 0 no se mueve en ese eje
-    Si el valor es 1 se mueve para abajo o la derecha
-  Por ejemplo:
-    Si la direccion es (-1,0) se esta moviendo hacia arriba
-    Si la direccion es (0,1) se esta moviendo hacia la derecha
-    Se excluyen los movimientos diagonales (-1,-1),(1,1),(-1,1),(1,-1) y el "no movimiento" (0,0)
+  Descripcion detellada de algunos conceptos:
+    En la matriz grid:
+      Si hay un 0 es una posicion vacía
+      Si hay un 1 es parte del cuerpo del Snake
+      Si hay un 2 es el punto que hay que recoger
+      Si hay un 3 es una pared
+
+    Para controlar la direccion se usa un pair en el que las posiciones son el incremento en la direccion Y,x.
+    Estos pueden tomar 3 valores:
+      Si el valor es -1 se mueve para arriba o para la izquierda
+      Si el valor es 0 no se mueve en ese eje
+      Si el valor es 1 se mueve para abajo o la derecha
+    Por ejemplo:
+      Si la direccion es (-1,0) se esta moviendo hacia arriba
+      Si la direccion es (0,1) se esta moviendo hacia la derecha
+      Se excluyen los movimientos diagonales (-1,-1),(1,1),(-1,1),(1,-1) y el "no movimiento" (0,0)
+
+    El cuerpo de la serpiente esta construido con un vector de pairs que representan las posiciones de la matriz
+    en los que se encuentra la serpiente en ese momento.
+    Para moverse la nueva posicion dada por la direccion en la que se esta moviendo es evaluada para conocer si
+    es una casilla valida a la que se puede mover, de ser asi esta casilla se agrega al frente del vector y se procede
+    a eliminar la ultima posicion del vecto para crear el "movimiemto" de la serpiente. Si la casilla a evaluar
+    es la casilla marcada como "punto" entonces no se elimima la ultima entrada del vector para asi hacer crecer la
+    serpiente y eumentar la dificultad. Por ultimo si la casilla a evaluar es una pared o es parte del cuerpo de
+    la serpiente el juego se da por concluido y se procede a imprimir la puntuacion total obtenida.
+
+    Para el manejo de la interfaz grafica se utiliza OpenCV (https://opencv.org/) debido a que se tiene previo
+    conocimento de esta biblioteca y por lo tanto resulta mas facil su uso. Esta biblioteca  se puede descargar
+    de https://opencv.org/releases.html.
+    Guia para instalacion https://docs.opencv.org/trunk/d7/d9f/tutorial_linux_install.html
+
+    La interfaz grafica consiste en una imagen en la que cada pixel corresponde a una entrada de la matriz grid
+    de manera que dependiendo de que haya en cada entrada de la matriz, se asocie un color a este pixel para poder
+    mostrar el juego al usuario.
 */
 
 #include<iostream>
-#include<vector>
+#include<vector> //Para el uso de vectores
 #include<cstdlib>
 #include<time.h>
-#include<utility>
-#include<opencv2/opencv.hpp>
+#include<utility> //Para el uso de Pairs
+#include<opencv2/opencv.hpp> //Biblioteca para el uso de interfaz gráfica
 
 using namespace cv;
 using namespace std;
@@ -30,15 +54,17 @@ using namespace std;
 
 class Solid{
   protected:
-    vector<vector<int> > grid;
-    pair<int,int> direction;
-    vector<pair<int,int> > snaku;
-    pair<int,int> prize;
-    int score;
-    bool live;
-    Mat img;
+    vector<vector<int> > grid; //Matriz encargada de almacenar toda la informacion del juego
+    pair<int,int> direction; //Este par indica la direccion en la que se mueve la serpiente
+    vector<pair<int,int> > snaku; //Vector en el que se guardan las casillas que forman parte del cuerpo de la serpiente
+    pair<int,int> prize; //Casilla objetivo que tiene que recoger la serpiente
+    int score; //Puntuacion
+    bool live; //Booleano encargado de decir si la serpiente esta viva (No ha perdido)
+    Mat img; //Imagen que se utiliza como interfaz grafica
 
   private:
+    /*Funcion encargada de escoher una nueva casilla para el objetivo.
+      Excluye las casillas que sean paredes o parte del cuerpo de la serpiente*/
     void setRandomPoint(void){
       srand(time(NULL));
       pair<int,int> point;
@@ -57,6 +83,7 @@ class Solid{
       prize = point;
     }
 
+    //Funcion encargada de reconstruir la matriz despues de cada movimiento
     void constructGrid(void){
       if(live){
         for(int i = 1; i < 17; i++){
@@ -76,6 +103,7 @@ class Solid{
     }
 
   public:
+    //Constructor de la clase, en este se incializan todas las variablesnecesarias para el funcionamiento del juego
     Solid(void){
       img = Mat(18, 32, CV_8UC3, Scalar(0,0, 0));
       score = 0;
@@ -109,16 +137,15 @@ class Solid{
         aux = snaku[i];
         grid[aux.first][aux.second] = 1;
       }
-      // constructGrid();
-      // gridToImage();
-      // printImage();
 
     }
 
+    //Destructor: No es necesario debido a que cada elemento usado posee se propio destructor implementado
     ~Solid(){
       cout<<"Gracias por Jugar"<<endl;
     }
 
+    //Funcion encargada de realizar los movimientos constantes de la serpiente
     void move(void){
       pair<int,int> newPosition;
       pair<int,int> aux = snaku.front();
@@ -142,6 +169,7 @@ class Solid{
       constructGrid();
     }
 
+    //Funcion encargada de modificar la direccion en la que se mueve la serpiente
     void changeDirection(int newDirection){
       if(newDirection == 119){
         direction = make_pair(-1,0);
@@ -157,8 +185,8 @@ class Solid{
       }
     }
 
+    //Funcion encargada de convertir la matriz grid a la imagen que se le va a presentar al usuario
     void gridToImage(void){
-      //Mat aux(18, 32, CV_8UC3, Scalar(0,0, 0));
       img = Mat(18, 32, CV_8UC3, Scalar(0,0, 0));
       Vec3b white(255,255,255);
       Vec3b blue(255,0,0);
@@ -175,23 +203,25 @@ class Solid{
           }
         }
       }
-      //img = aux;
     }
 
+    //Funcion encargada de imprimir la imagen en pantalla
     void printImage(void){
       resize(img, img, Size(), 25, 25, INTER_NEAREST);
-      //namedWindow("Snaku", CV_WINDOW_AUTOSIZE);
       imshow("Snaku", img);
     }
 
+    //Funcion encargada de regresar si todavia no se ha perdido
     bool isAlive(void){
       return live;
     }
 
+    //Funcion encargada de imprimir la puntuacion final del jugador
     void printScore(void){
       cout<<"Score: "<<score<<endl;
     }
 
+    //Destructor de la imagen
     void destroyImage(void){
       destroyWindow("Snaku");
     }
